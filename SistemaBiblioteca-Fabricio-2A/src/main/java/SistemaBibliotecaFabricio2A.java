f3ef2b21cabd401bef6522eebbe673bda14cc70a
Fabricio <fabriciomarquesdacunha.2710@gmail.com>
2025-04-22 12:29:19 -0300

    Add files via upload

---
+++ SistemaBiblioteca-Fabricio-2A/src/main/java/SistemaBibliotecaFabricio2A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 2024105070029
+ */
+public class SistemaBibliotecaFabricio2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
